605e629c140eb3c61b752dc00189c3ff0c99b457
Micah Thornton <48172545+micahtml@users.noreply.github.com>
2023-03-05 15:59:46 -0600

    clean, preprocess, perform EDA

--- COVID-19 Tracker/main.py
+++ COVID-19 Tracker/main.py
@@ -1,5 +1,20 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 
 url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/' \
     'csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
 df = pd.read_csv(url)
+
+# drop rows with missing data
+df.dropna(inplace=True)
+
+# aggregate the data at the country level
+df = df.groupby(['United States']).sum()
+
+# plot the total number of confirmed cases by country
+df_total = df.iloc[:, -1].sort_values(ascending=False)
+df_total.plot(kind='bar', figsize=(15, 5))
+plt.title('Total confirmed cases by country')
+plt.xlabel('Country')
+plt.ylabel('Confirmed cases')
+plt.show()
