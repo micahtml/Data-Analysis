5a84eea1001ab10a1ed8976a936f82ce10591e0f
Micah Thornton <48172545+micahtml@users.noreply.github.com>
2023-03-05 15:56:23 -0600

    Import the dataset

---
+++ COVID-19 Tracker/main.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/' \
+    'csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+df = pd.read_csv(url)
