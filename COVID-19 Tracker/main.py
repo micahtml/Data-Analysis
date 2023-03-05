11ea94d837140fa2ae2d143024dfb59751d4bc5c
Micah Thornton <48172545+micahtml@users.noreply.github.com>
2023-03-05 16:27:48 -0600

    update for United States stats

--- COVID-19 Tracker/main.py
+++ COVID-19 Tracker/main.py
@@ -1,19 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 import plotly.express as px
 import scipy.stats as stats
-import matplotlib.pyplot as plt
-import pandas as pd
 
+# Import the COVID-19 dataset from John Hopkins University
 url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/' \
     'csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
 df = pd.read_csv(url)
 
-# drop rows with missing data
+# Clean and preprocess the data
 df.dropna(inplace=True)
+df = df.groupby(['Country/Region']).sum()
 
-# aggregate the data at the country level
-df = df.groupby(['Country/region']).sum()
+# Accommodate for the United States as a country
+us_data = df.loc['US']
+us_data.index = pd.to_datetime(us_data.index)
+us_data = us_data.resample('D').sum().fillna(method='ffill')
+df.drop('US', inplace=True)
+df.loc['United States'] = us_data
 
-# plot the total number of confirmed cases by country
+# Perform exploratory data analysis
 df_total = df.iloc[:, -1].sort_values(ascending=False)
 df_total.plot(kind='bar', figsize=(15, 5))
 plt.title('Total confirmed cases by country')
@@ -21,16 +27,14 @@ plt.xlabel('Country')
 plt.ylabel('Confirmed cases')
 plt.show()
 
-
-# perform a t-test to compare the total number of confirmed cases between two countries
-country1 = df.loc['US']
-country2 = df.loc['China']
+# Perform statistical analysis
+country1 = df.loc['Italy']
+country2 = df.loc['United States']
 t, p = stats.ttest_ind(country1, country2)
 print('t-statistic:', t)
 print('p-value:', p)
 
-
-# create a choropleth map to visualize the total number of confirmed cases by country
+# Communicate your findings
 fig = px.choropleth(df_total.reset_index(), locations='Country/Region', locationmode='country names',
                     color=df_total.values, hover_name='Country/Region', color_continuous_scale='Reds',
                     title='Total confirmed cases by country')
