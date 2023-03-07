8c457f4848da545db5f79b6d71166e5bea050a35
Micah Thornton <48172545+micahtml@users.noreply.github.com>
2023-03-06 21:32:12 -0600

    update purpose

--- basketball-reference.com/main.py
+++ basketball-reference.com/main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Define the URL for the NBA season stat leaders data
 url = 'https://www.basketball-reference.com/leagues/NBA_2022_totals.html'
@@ -22,9 +23,55 @@ df.drop_duplicates(inplace=True)
 df.dropna(how='all', inplace=True)
 
 # Convert the relevant columns to numeric data type
-numeric_cols = ['MP', 'FG', 'FGA', '3P', '3PA', 'FT', 'FTA',
+numeric_cols = ['G', 'MP', 'FG', 'FGA', '3P', '3PA', 'FT', 'FTA',
                 'ORB', 'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS']
 df[numeric_cols] = df[numeric_cols].apply(pd.to_numeric, errors='coerce')
 
-# Print the cleaned DataFrame
-print(df)
+# Select the top 10 scorers and sort by points in descending order
+df = df[['Player', 'Pos', 'Age', 'Tm', 'PTS']].sort_values(
+    'PTS', ascending=False).head(10)
+
+# Create a bar chart of the top scorers
+plt.bar(df['Player'], df['PTS'])
+plt.title('Top 10 Scorers in NBA 2022 Season')
+plt.xlabel('Player')
+plt.ylabel('Total Points')
+plt.show()
+
+# Select the top performers in each major stat category
+rebounds_leader = df[['Player', 'TRB']].sort_values(
+    'TRB', ascending=False).iloc[0]
+assists_leader = df[['Player', 'AST']].sort_values(
+    'AST', ascending=False).iloc[0]
+steals_leader = df[['Player', 'STL']].sort_values(
+    'STL', ascending=False).iloc[0]
+blocks_leader = df[['Player', 'BLK']].sort_values(
+    'BLK', ascending=False).iloc[0]
+
+# Create a bar chart of the rebounds leader
+plt.bar(rebounds_leader['Player'], rebounds_leader['TRB'])
+plt.title('Rebounds Leader in NBA 2022 Season')
+plt.xlabel('Player')
+plt.ylabel('Total Rebounds')
+plt.show()
+
+# Create a bar chart of the assists leader
+plt.bar(assists_leader['Player'], assists_leader['AST'])
+plt.title('Assists Leader in NBA 2022 Season')
+plt.xlabel('Player')
+plt.ylabel('Total Assists')
+plt.show()
+
+# Create a bar chart of the steals leader
+plt.bar(steals_leader['Player'], steals_leader['STL'])
+plt.title('Steals Leader in NBA 2022 Season')
+plt.xlabel('Player')
+plt.ylabel('Total Steals')
+plt.show()
+
+# Create a bar chart of the blocks leader
+plt.bar(blocks_leader['Player'], blocks_leader['BLK'])
+plt.title('Blocks Leader in NBA 2022 Season')
+plt.xlabel('Player')
+plt.ylabel('Total Blocks')
+plt.show()
