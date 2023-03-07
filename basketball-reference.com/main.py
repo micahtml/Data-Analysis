b5ede70c1e3f10e26ac0a296bfcd1f80757051f7
Micah Thornton <48172545+micahtml@users.noreply.github.com>
2023-03-07 00:56:12 -0600

    refactor

--- basketball-reference.com/main.py
+++ basketball-reference.com/main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.graph_objs as go
 
 # Define the URL for the NBA season stat leaders data
-url = 'https://www.basketball-reference.com/leagues/NBA_2022_totals.html'
+url = 'https://www.basketball-reference.com/leagues/NBA_2023_totals.html'
 
 # Use pandas to read the HTML table into a DataFrame
 df = pd.read_html(url)[0]
@@ -23,25 +23,21 @@ df.drop_duplicates(inplace=True)
 df.dropna(how='all', inplace=True)
 
 # Convert the relevant columns to numeric data type
-numeric_cols = ['G', 'MP', 'FG', 'FGA', '3P', '3PA', 'FT', 'FTA',
-                'ORB', 'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS']
+numeric_cols = ['G', 'MP', 'FG', 'FGA', '3P', '3PA', 'FT', 'FTA', 'ORB', 'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS']
 df[numeric_cols] = df[numeric_cols].apply(pd.to_numeric, errors='coerce')
 
 # Calculate a new "MVP score" column
-df['MVP score'] = (df['PTS'] / df['FGA']) + (df['TRB'] /
-                                             df['DRB']) + (df['AST'] / df['TOV'])
+df['MVP score'] = (df['PTS'] / df['FGA']) + (df['TRB'] / df['DRB']) + (df['AST'] / df['TOV'])
 
 # Select the top 5 MVP candidates and sort by MVP score in descending order
-mvp_candidates = df[['Player', 'Pos', 'Age', 'Tm', 'PTS', 'TRB', 'AST',
-                     'MVP score']].sort_values('MVP score', ascending=False).head(5)
+mvp_candidates = df[['Player', 'Pos', 'Age', 'Tm', 'PTS', 'TRB', 'AST', 'MVP score']].sort_values('MVP score', ascending=False).head(5)
 
 # Create a bar chart of the top MVP candidates
 fig = go.Figure(data=[
     go.Bar(name='Points', x=mvp_candidates['Player'], y=mvp_candidates['PTS']),
-    go.Bar(name='Rebounds',
-           x=mvp_candidates['Player'], y=mvp_candidates['TRB']),
+    go.Bar(name='Rebounds', x=mvp_candidates['Player'], y=mvp_candidates['TRB']),
     go.Bar(name='Assists', x=mvp_candidates['Player'], y=mvp_candidates['AST'])
 ])
 fig.update_layout(
-    barmode='group', title='Top 5 MVP Candidates in NBA 2022 Season')
+    barmode='group', title='Top 5 MVP Candidates in NBA 2022-23 Season')
 fig.show()
