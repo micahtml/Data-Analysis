6049fb0ff8d829b9e53b53c7ff4567afe9c9157b
Micah Thornton <48172545+micahtml@users.noreply.github.com>
2023-03-12 18:03:00 -0500

    v2

--- Suggestion Engine/mood_assesment.py
+++ Suggestion Engine/mood_assesment.py
@@ -1,42 +1,40 @@
-import random
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
-# Define our list of moods and behaviors, along with example activities
-mood_behavior_activities = {
-    "happy": {
-        "listening to music",
-        "going for a walk",
-        "hanging out with friends",
-        "watching a comedy movie"
-    },
-    "sad": {
-        "taking a warm bath",
-        "writing in a journal",
-        "reading a favorite book",
-        "watching a comforting movie"
-    },
-    "stressed": {
-        "meditating",
-        "doing some yoga",
-        "taking a nap",
-        "listening to calming music"
-    },
-    "tired": {
-        "taking a power nap",
-        "doing some light exercise",
-        "drinking some caffeine",
-        "going for a brisk walk"
-    }
-}
+# Load the dataset
+df = pd.read_csv("mood_dataset.csv")
 
-# Prompt the user to enter their mood and behavior
-mood = input("How are you feeling? (happy, sad, stressed, tired): ")
-behavior = input(
-    "What have you been doing? (working, studying, exercising, relaxing): ")
+# Preprocess the data
+vectorizer = CountVectorizer(stop_words="english")
+X = vectorizer.fit_transform(df["text"])
+y = df["mood"]
 
-# Check if the user's mood and behavior match any of our entries, and suggest an activity if so
-if mood in mood_behavior_activities and behavior in mood_behavior_activities[mood]:
-    suggestions = mood_behavior_activities[mood][behavior]
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
+
+# Train a support vector machine (SVM) model on the training data
+model = SVC(kernel="linear", C=0.5)
+model.fit(X_train, y_train)
+
+# Evaluate the model on the testing data
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+
+# Use the model to predict the mood from a new text input
+text_input = input("How are you feeling? ")
+text_input_vectorized = vectorizer.transform([text_input])
+mood_prediction = model.predict(text_input_vectorized)
+print("You seem to be feeling", mood_prediction[0])
+
+# Use the predicted mood to provide personalized suggestions
+if mood_prediction[0] in mood_behavior_activities:
+    suggestions = mood_behavior_activities[mood_prediction[0]]
     suggestion = random.choice(list(suggestions))
-    print("If you're feeling", mood, "and you've been", behavior + ", try", suggestion + "!")
+    print("If you're feeling", mood_prediction[0], "try", suggestion + "!")
 else:
     print("Take a break and do something that makes you happy!")
